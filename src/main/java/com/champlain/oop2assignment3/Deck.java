f0f024b678e7536c89aceb6f754a92018fcf3a22
AJ <AlainJunior.McCutche@stu.crc-lennox.qc.ca>
2024-10-06 11:28:41 -0400

    Created the SuitFirstComparator feature

--- src/main/java/com/champlain/oop2assignment3/Deck.java
+++ src/main/java/com/champlain/oop2assignment3/Deck.java
@@ -1,9 +1,6 @@
 package com.champlain.oop2assignment3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Represents a deck of playing cards.
@@ -55,5 +52,12 @@ public class Deck extends CardCollection implements CardSource {
      */
     public Iterator<Card> iterator() {
         return this.aCards.iterator();
+    }
+
+    /**
+     * Sorts the deck of cards by suit first, then by rank.
+     */
+    public void sortBySuit() {
+        this.aCards.sort(Comparator.comparing(Card::getSuit).thenComparing(Card::getRank));
     }
 }
